a353fa5f4dc5c2b31cd92d3b2067fcb25be2025c
Ryu Alvano <117072949+BlvckM1nds@users.noreply.github.com>
2024-12-27 00:12:30 +0700

    practicing bitstring and binaries

--- lib/bitstring_binaries.ex
+++ lib/bitstring_binaries.ex
@@ -7,23 +7,69 @@ defmodule BitstringBinaries do
 
   def empty_picture(), do: <<>>
 
-  def test_picture() do
-    # Please implement the test_picture/0 function
+  def test_picture(), do: <<0::2, 1::2, 2::2, 3::2>>
+
+  def prepend_pixel(picture, color_count, pixel_color_index) do
+    bit_size = palette_bit_size(color_count)
+    <<pixel_color_index::size(bit_size), picture::bitstring>>
   end
 
-  # def prepend_pixel(picture, color_count, pixel_color_index) do
-  #   # Please implement the prepend_pixel/3 function
-  # end
+  defguardp no_picture(picture) when picture == <<>>
 
-  # def get_first_pixel(picture, color_count) do
-  #   # Please implement the get_first_pixel/2 function
-  # end
+  def get_first_pixel(picture, _color_count) when no_picture(picture), do: nil
 
-  # def drop_first_pixel(picture, color_count) do
-  #   # Please implement the drop_first_pixel/2 function
-  # end
+  def get_first_pixel(picture, color_count) do
+    bit_size = palette_bit_size(color_count)
+    <<first_pixel::size(bit_size), _rest::bitstring>> = picture
+    first_pixel
+  end
 
-  # def concat_pictures(picture1, picture2) do
-  #   # Please implement the concat_pictures/2 function
-  # end
+  def drop_first_pixel(picture, _color_count) when no_picture(picture), do: <<>>
+
+  def drop_first_pixel(picture, color_count) do
+    bit_size = palette_bit_size(color_count)
+    <<_first_pixel::size(bit_size), rest::bitstring>> = picture
+    rest
+  end
+
+  def concat_pictures(picture1, picture2) when no_picture(picture1) and no_picture(picture2),
+    do: <<>>
+
+  def concat_pictures(picture1, picture2) when no_picture(picture2), do: picture1
+  def concat_pictures(picture1, picture2) when no_picture(picture1), do: picture2
+  def concat_pictures(picture1, picture2), do: <<picture1::bitstring, picture2::bitstring>>
+
+  defmodule DNA do
+    def encode_nucleotide(code_point) do
+      case code_point do
+        ?A -> 0b0001
+        ?C -> 0b0010
+        ?G -> 0b0100
+        ?T -> 0b1000
+        " " -> 0b0000
+      end
+    end
+
+    def decode_nucleotide(encoded_code) do
+      case encoded_code do
+        0b0001 -> ?A
+        0b0010 -> ?C
+        0b0100 -> ?G
+        0b1000 -> ?T
+        0b0000 -> " "
+      end
+    end
+
+    def encode(dna) do
+      dna
+      |> Enum.map(&encode_nucleotide/1)
+      |> Enum.reduce(<<>>, fn encoded, acc -> <<acc::bitstring, encoded::4>> end)
+    end
+
+    def decode(dna) do
+      dna
+      |> Enum.map(&decode_nucleotide/1)
+      |> Enum.reduce(<<>>, fn decoded, acc -> <<acc::bitstring, decoded::8>> end)
+    end
+  end
 end
