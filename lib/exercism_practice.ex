a909be46099a3a270a6441ef5f1c00d8599d6ca3
Ryu Alvano <117072949+BlvckM1nds@users.noreply.github.com>
2024-12-30 00:11:31 +0700

    working on date struct

--- lib/exercism_practice.ex
+++ lib/exercism_practice.ex
@@ -112,4 +112,49 @@ defmodule ExercismPractice do
     )
     |> Enum.map(&elem(&1, 1))
   end
+
+  defmodule LibraryFees do
+    def datetime_from_string(string) do
+      case NaiveDateTime.from_iso8601(string) do
+        {:ok, naive_datetime} -> naive_datetime
+        {:error, _reason} -> :error
+      end
+    end
+
+    def before_noon?(datetime), do: datetime.hour < 12
+
+    def return_date(%{year: year, month: month, day: day} = checkout_datetime) do
+      checkout_date = Date.new!(year, month, day)
+
+      if before_noon?(checkout_datetime) do
+        Date.add(checkout_date, 28)
+      else
+        Date.add(checkout_date, 29)
+      end
+    end
+
+    def days_late(planned_return_date, actual_return_datetime) do
+      actual_return_datetime
+      |> Date.diff(planned_return_date)
+      |> max(0)
+    end
+
+    def monday?(%{year: year, month: month, day: day}) do
+      formatted_date = Date.new!(year, month, day)
+      Date.day_of_week(formatted_date) == 1
+    end
+
+    def calculate_late_fee(checkout, return, rate) do
+      checkout_date = datetime_from_string(checkout)
+      return_date = datetime_from_string(return)
+
+      raw_fee = days_late(return_date(checkout_date), return_date) * rate
+
+      if monday?(return_date) do
+        floor(raw_fee * 0.5)
+      else
+        raw_fee
+      end
+    end
+  end
 end
